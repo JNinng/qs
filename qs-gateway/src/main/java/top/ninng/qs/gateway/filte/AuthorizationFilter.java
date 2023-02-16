41239a32eddc7149c271ef31b96b8951f2fefee8
OhmLaw <xx1599xx@163.com>
2023-02-16 13:07:54 +0800

    feat(qs-user): 添加网关授权过滤器

---
+++ qs-gateway/src/main/java/top/ninng/qs/gateway/filte/AuthorizationFilter.java
@@ -0,0 +1,27 @@
+package top.ninng.qs.gateway.filte;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author OhmLaw
+ * @Date 2023/2/16 11:18
+ * @Version 1.0
+ */
+@Component
+@Order(0)
+public class AuthorizationFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpRequest.Builder mutate = request.mutate();
+        ServerHttpRequest build = mutate.header("gateway_token", "6ff71f1a-68ec-4f76-b0c3-e39882e574a0").build();
+        return chain.filter(exchange.mutate().request(build).build());
+    }
+}
